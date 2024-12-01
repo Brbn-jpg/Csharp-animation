acb268348ca1dd8de956cf23db8570d383b631f0
Brbn-jpg <kubon.kuznicki@gmail.com>
2024-12-01 01:04:38 +0100

    initial commit

---
+++ animacjaKomputerowa/AnimacjaKomputerowa/AnimacjaKomputerowa/Animacja generowana komputerowo.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace AnimacjaKomputerowa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.start = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.restart = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // start
+            // 
+            this.start.Image = ((System.Drawing.Image)(resources.GetObject("start.Image")));
+            this.start.Location = new System.Drawing.Point(965, 47);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(47, 47);
+            this.start.TabIndex = 0;
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(52, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(880, 621);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // restart
+            // 
+            this.restart.Image = ((System.Drawing.Image)(resources.GetObject("restart.Image")));
+            this.restart.Location = new System.Drawing.Point(965, 100);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(47, 47);
+            this.restart.TabIndex = 3;
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(938, 196);
+            this.trackBar1.Maximum = 8;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(112, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 1;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(962, 180);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ilość piłek: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1053, 701);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.start);
+            this.Name = "Form1";
+            this.Text = "Animacja generowana komputerowo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ animacjaKomputerowa/AnimacjaKomputerowa/AnimacjaKomputerowa/Animacja generowana komputerowo.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AnimacjaKomputerowa
+{
+    public partial class Form1 : Form
+    {
+        int ilosc;
+        Graphics graphics;
+        Pen pen;
+        float height;
+        int interval = 10;
+        float gravity = 0.2f;
+        private bool animationRunning = false;
+
+        List<Ball> balls = new List<Ball>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            pen = new Pen(Color.Black);
+        }
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            int ilosc = trackBar1.Value;
+            label1.Text = "ilość piłek: " + ilosc.ToString();
+        }
+        private void start_Click(object sender, EventArgs e)
+        {
+            ilosc = trackBar1.Value;
+            if (animationRunning)
+            {
+                return;
+            }
+            balls.Clear();
+
+            Random random = new Random();
+            
+            // Dodajemy kilka piłek do listy
+            for (int i = 0; i < ilosc; i++)
+            {
+                float x0 = random.Next(0, pictureBox1.Width - 35); 
+                float y0 = random.Next(0, pictureBox1.Height / 2); 
+                float angle = (float)(random.NextDouble() * Math.PI / 2); 
+                float speed = random.Next(2, 5); 
+
+                float velocityX = speed * (float)Math.Cos(angle);
+                float velocityY = speed * (float)Math.Sin(angle);
+
+                Color color = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256)); 
+
+                Ball ball = new Ball(x0, y0, velocityX, velocityY, color); 
+                balls.Add(ball);
+            }
+
+            height = 35f;
+            graphics = pictureBox1.CreateGraphics();
+            animationRunning = true;
+
+            Task.Run(() =>
+            {
+                do
+                {
+                    bool allBallsStopped = true; 
+
+                    foreach (Ball ball in balls)
+                    {
+                        Brush brush = new SolidBrush(ball.Color);
+                        graphics.FillEllipse(brush, ball.X, ball.Y, 35f, 35f);
+                        ball.VelocityY += gravity;
+                        ball.Y += ball.VelocityY;
+
+                        // Sprawdzamy kolizje z krawędziami
+                        if (ball.Y >= pictureBox1.Height - height - 3) 
+                        {
+                            ball.Y = pictureBox1.Height - height - 3;
+                            ball.VelocityY *= -0.8f; 
+                        }
+                        else if (ball.Y <= 0) 
+                        {
+                            ball.Y = 0;
+                            ball.VelocityY *= -0.8f; 
+                        }
+
+                        if (ball.X <= 0 || ball.X >= pictureBox1.Width - 35)
+                        {
+                            ball.VelocityX *= -1; 
+                        }
+                        if (Math.Abs(ball.VelocityY) > 0.1f || Math.Abs(ball.VelocityX) > 0.1f)
+                        {
+                            allBallsStopped = false; 
+                        }
+                    }
+
+                    graphics.DrawLine(pen, 4, pictureBox1.Height - 3, pictureBox1.Width - 5, pictureBox1.Height - 3);
+                    System.Threading.Thread.Sleep(interval);
+                    graphics.Clear(pictureBox1.BackColor);
+
+                    if (allBallsStopped)
+                    {
+                        animationRunning = false;
+                        break;
+                    }
+
+                } while (animationRunning);
+            });
+        }
+        // Klasa reprezentująca piłkę
+        public class Ball
+        {
+            public float X { get; set; }
+            public float Y { get; set; }
+            public float VelocityX { get; set; }
+            public float VelocityY { get; set; }
+            public Color Color { get; set; }
+
+            public Ball(float x, float y, float velocityX, float velocityY, Color color)
+            {
+                X = x;
+                Y = y;
+                VelocityX = velocityX;
+                VelocityY = velocityY;
+                Color = color;
+            }
+        }
+
+
+        private void restart_Click(object sender, EventArgs e)
+        {
+            animationRunning = false;
+            balls.Clear();
+            graphics.Clear(pictureBox1.BackColor);
+        }
+
+
+    }
+}
